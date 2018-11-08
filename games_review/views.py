444202d12d41ab1b813d8f6b9881b64eb462e560
DELEAU Adrien <deleaua@tp3-pc08u.info.iutamiens>
2018-11-08 17:15:55 +0100

    TD9 et TD10 Fini , en attente du prochain TP...

--- games_review/views.py
+++ games_review/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from games_review.models import Game, Type
 
 # Create your views here.
+
+def index(request):
+    games = Game.objects.all()
+    return render(request, 'index.html', {'latests_games': games})
+
+def game(request, slug):
+    game = get_object_or_404(Game, slug=slug)
+    return render(request, 'game.html', {'game': game})
